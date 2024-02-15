9e35de7b0e5a11fc549fe5762f84206dc95a29e4
HeRo3S <hungid6a1@gmail.com>
2024-02-15 19:14:43 +0700

    finished creating account script

--- setups/main.sh
+++ setups/main.sh
@@ -3,9 +3,13 @@
 script_dir=$(dirname "$(realpath "$0")")
 parent_dir=$(dirname "$script_dir")
 
-# NOTE:create user/groups (for minimalistic distro)
-chmod +x "$script_dir"/user.sh
-# source "$(pwd)"/user.sh
+# NOTE:create user/groups (for installation without creating account before)
+read -r -p "Do you want to create a user account? (y/n): " create_account
+
+if [[ $create_account == "y" || $create_account == "Y" ]]; then
+    chmod +x "$script_dir"/user.sh
+    source "$script_dir"/user.sh
+fi
 
 # NOTE:install packages 
 chmod +x "$script_dir"/packages.sh
@@ -17,3 +21,4 @@ stow "$parent_dir" -t ~/
 # TODO:setup X11/wayland
 # TODO:setup windows manager
 # TODO:setup services/deamon
+
--- setups/user.sh
+++ setups/user.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-useradd -m -G wheel amelia
-# TODO: setup sudo permission for wheel group
+script_dir=$(dirname "$(realpath "$0")")
+parent_dir=$(dirname "$script_dir")
+
+read -r -p "Typing username: " USERNAME
+read -r -p "Typing groupname: " GROUPNAME
+
+useradd -m -s /bin/bash -G "$GROUPNAME" "$USERNAME"
+
+# Set the user's password (optional)
+echo "$USERNAME:password" | sudo chpasswd
+
+# Add sudoers privileges to the group
+echo "%$GROUPNAME ALL=(ALL) ALL" | sudo tee /etc/sudoers.d/"$GROUPNAME"
+
+# Move the script repository to the new user's home directory
+mv "$parent_dir" /home/"$USERNAME"/
+
+# Change ownership of the script repository to the new user
+chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/"$(basename "$parent_dir")"
+
+su - "$USERNAME"
