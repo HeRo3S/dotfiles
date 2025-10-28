2989ab415cf6e25c03d0ee5d43a2de6ecf3f8034
HeRo3S <hungid6a1@gmail.com>
2025-10-28 09:43:43 +0700

    (feat): add chromium for development purpose

--- nixos/modules/home-manager/essentials/browser/default.nix
+++ nixos/modules/home-manager/essentials/browser/default.nix
@@ -1,6 +1,7 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [ inputs.zen-browser.homeModules.twilight ];
   programs.zen-browser.enable = true;
+  home.packages = [ pkgs.chromium ];
 }
